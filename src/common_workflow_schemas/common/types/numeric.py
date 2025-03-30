ba0c72cebe815e36c43b82384eda9bc1d42e6607
Edan Bainglass <edan.bainglass@gmail.com>
2025-03-30 09:13:25 +0000

    Add JsonSchema definition for `FloatArray`

--- src/common_workflow_schemas/common/types/numeric.py
+++ src/common_workflow_schemas/common/types/numeric.py
@@ -1,4 +1,15 @@
+import typing as t
+
 import numpy as np
 import numpy.typing as npt
+import pydantic as pdt
 
-FloatArray = npt.NDArray[np.float64]
+FloatArray = t.Annotated[
+    npt.NDArray[np.float64],
+    pdt.WithJsonSchema(
+        {
+            "type": "array",
+            "items": {"type": "number"},
+        }
+    ),
+]
